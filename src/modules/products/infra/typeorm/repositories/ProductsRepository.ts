1c82525b9d761faef93552cc9d2fb492f1f9c522
guilhermeaw <guilherme.w@universo.univates.br>
2020-06-06 14:14:56 -0300

    Fix missing await on async function

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -33,7 +33,7 @@ class ProductsRepository implements IProductsRepository {
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    const product = this.ormRepository.findOne({
+    const product = await this.ormRepository.findOne({
       where: { name },
     });
 
